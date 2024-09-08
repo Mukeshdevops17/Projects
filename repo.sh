be0ba05c85675a283a5889d61a7540b342eea809
mukesh <mukeshgundu@gmail.com>
2024-09-08 09:29:24 +0000

    first project

---
+++ repo.sh
@@ -0,0 +1,5 @@
+curl -L \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: 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" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/orgs/dehtesting/repos
